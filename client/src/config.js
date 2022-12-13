11cecd4a9b6321a9fd4f4647cd1b3ef7a70940a5
Joel Karacozoff <joel.karacozoff@gmail.com>
2022-12-13 14:54:23 -0500

    trying new base url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://amygdala.herokuapp.com/";
+let BASE_URL = "https://amygdala.herokuapp.com/amygdala/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
