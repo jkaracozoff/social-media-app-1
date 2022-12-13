793a0df3b6d48d5d7cdfdcd8a234c36397685a2e
Joel Karacozoff <joel.karacozoff@gmail.com>
2022-12-13 15:01:33 -0500

    switching back to post it base url

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://amygdala.herokuapp.com/amygdala/";
+let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
